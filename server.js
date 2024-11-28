250e0a2c8a74c694dda744254f50d11ad2a11855
ayush7078 <ayushporwal7078@gmail.com>
2024-11-28 20:47:24 +0530

    New Changes

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use(errorHandlerMiddleware);
 
+app.get('/', (req, res) => {
+  res.send("Server is running");
+});
+ss
 // Serve Swagger API Documentation at /api-docs
 const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'swagger/swagger.json'), 'utf8'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -36,4 +40,4 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Connect to the database and start the server
 connectDB();
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () =>{ return "Server is running"});
+server.listen(PORT, () =>{  console.log(`Server is running on 5000ss`)});
