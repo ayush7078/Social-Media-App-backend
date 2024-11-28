a9b57234db26040593661944c247dc3f8984c6f0
ayush7078 <ayushporwal7078@gmail.com>
2024-11-28 20:52:23 +0530

    New Changes

--- server.js
+++ server.js
@@ -40,4 +40,4 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Connect to the database and start the server
 connectDB();
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () =>{  console.log(`Server is running on 5000ss`)});
+server.listen(PORT, () =>  console.log("Server is running"));
